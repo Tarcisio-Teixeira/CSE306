95e5f73d072087be3feea7d8639657b318b6144f
Tarcisio Soares Teixeira Neto <tarcisioteixeira@Tarcisios-MacBook-Air.local>
2021-06-19 12:00:19 +0200

    big fluid simulation

--- Assigment3/main.cpp
+++ Assigment3/main.cpp
@@ -10,7 +10,7 @@ int main(){
     // PowerDiagram::createPowerDiagram(500, "voronois/powerDiagram.svg" );
     // VoronoiDiagram::createVoronoiDiagram(10000, false, "voronois/voronoiDiagram.svg");
     // VoronoiDiagram::createVoronoiDiagram(1000, true, "voronois/centeredVoronoiDiagram.svg");
-    // FluidSimulation::fluidSimulation(2500,700,5000);
+    FluidSimulation::fluidSimulation(2500,700,5000);
     // OptimalTransportSimulation::optimalTransportSimulation(1000);
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
